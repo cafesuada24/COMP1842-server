da9f63bd0b39b1e0ad3641776c39244494d356a3
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-11-24 04:30:30 +0700

    Update exporting

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,16 +1,10 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
+export async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    })
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
   }
 };
-
-module.exports = connectDB;
