be77a1d03fcfee5fe659109a47080c6f23edf6ff
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-11-24 04:30:42 +0700

    Update entry

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 import express from "express";
 import router from "./features/auth/authRoutes.js";
+import { connectDB } from "./config/dbConfig.js";
+await connectDB();
+
 const app = express();
 
 app.use('/api/auth', router);
-export default app;
+//export default app;
+app.listen(process.env.PORT, () => {
+    console.log(`App is listening at http://localhost:${process.env.PORT}`)
+})
