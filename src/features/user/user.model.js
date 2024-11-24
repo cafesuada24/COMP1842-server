71c7aaa7463241f8418e867ca923e25bf670bafe
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-11-24 20:32:52 +0700

    Update user model

--- src/features/user/user.model.js
+++ src/features/user/user.model.js
@@ -3,6 +3,10 @@ import { Schema, model } from 'mongoose';
 import bcrypt from 'bcryptjs';
 
 const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true,
@@ -13,14 +17,40 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
-  name: {
-    type: String,
-    required: true,
-  },
   role: {
     type: String,
     default: 'user',
     enum: ['user', 'admin'],
+  },
+  preferences: {
+    currency: {
+      type: String,
+      enum: ['USD', 'VND'],
+      default: 'USD',
+    },
+    language: {
+      type: String,
+      enum: ['en', 'vi'],
+      default: 'en',
+    },
+  },
+  finacialSummary: {
+    totalIncome: {
+      type: Number,
+      default: 0,
+    },
+    totalExpenses: {
+      type: Number,
+      default: 0,
+    },
+    savingGoal: {
+      type: Number,
+      default: 0,
+    }
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   },
 }, { timestamps: true });
 
