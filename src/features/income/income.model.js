688c83e3a4114f85e086294f62de90eefcaa9423
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-11-24 21:07:36 +0700

    Add income model

---
+++ src/features/income/income.model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const incomeCategorySchema = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const incomeSchema = new Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'incomeCategory',
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+const incomeCategoryModel = model('incomeCategory', incomeCategorySchema);
+const incomeModel = model('income', incomeSchema);
+
+export { incomeModel, incomeCategoryModel };
