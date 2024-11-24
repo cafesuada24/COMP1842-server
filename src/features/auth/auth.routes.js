4df573a5fdcb8ba233c4a6842cf0940b16d580c2
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-11-24 20:32:30 +0700

    remove profile route

--- src/features/auth/auth.routes.js
+++ src/features/auth/auth.routes.js
@@ -1,13 +1,9 @@
 import { Router } from 'express';
 import { register, login } from './auth.controller.js';
-import { verifyUser } from './auth.middleware.js';
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/profile', verifyUser, (req, res) => {
-    res.status(200).json({ user: req.user });
-});
 
 export default router;
